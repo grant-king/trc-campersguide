d0e626d4940ba125c300fca0b4ac093364a338fc
Grant King <grantryan.king@gmail.com>
2024-01-16 09:34:50 -0800

    Update TopHeader image
    Create landing.webp asset image. Update TopHeader component to use it.

--- vue-project/src/components/TopHeader.vue
+++ vue-project/src/components/TopHeader.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="-mx-4 -mt-10">
-        <div class="bg-cover bg-center bg-[url('@/assets/camping_1.webp')]">
+        <div class="bg-cover bg-center bg-[url('@/assets/landing.webp')]">
             <div class="h-dvh flex justify-center items-center bg-gradient-to-t from-black/80 via-white/80 to-white/95">
                 <h1 class="text-6xl font-bold text-center drop-shadow-2xl shadow-sm bg-clip-text text-transparent bg-gradient-to-r from-green-900 via-lime-700 to-lime-800">The Unofficial TRC Quick Reference</h1>
             </div>
@@ -9,5 +9,5 @@
 </template>
 
 <script setup>
-import camping_1 from '@/assets/camping_1.webp'
+
 </script>
